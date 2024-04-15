750a0d8f5c4076cc0a06f284ffdcebbeeec564c5
joyce0129 <131430612+joyce0129@users.noreply.github.com>
2024-04-15 20:38:24 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -6,12 +6,32 @@ import Navigation from "./src/navigation";
 import "react-native-gesture-handler";
 import { GluestackUIProvider, StatusBar } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
-//import HomeScreen from "./src/screens/HomeScreen";
-//import Header from "./src/components/Header";
+
+import { useFonts } from "expo-font";
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
 const App =()=>{
+   //引入字體
+   const [fontsLoaded, fontError] = useFonts({
+    "cjkFonts": require("./assets/fonts/cjkFonts_allseto_v1.11.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return(
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}} onLayout={onLayoutRootView}>
         <GluestackUIProvider config={config}>
           <StatusBar />
           <Navigation />
